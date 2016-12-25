c9b0dd89093dcd06c7faab51c1f76b4a5d0c17c5
Ben <Ben@B-PC>
2016-12-25 11:56:36 +1030

    Start Of GIthub

--- WindowsApplication2/Main.Designer.vb
+++ WindowsApplication2/Main.Designer.vb
@@ -22,37 +22,51 @@ Partial Class Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.Loader = New System.Windows.Forms.TabPage()
+        Me.txt_setmodname = New System.Windows.Forms.TextBox()
+        Me.btn_modname = New System.Windows.Forms.Button()
+        Me.btn_removemod = New System.Windows.Forms.Button()
         Me.btn_AddMod = New System.Windows.Forms.Button()
         Me.btn_modlistdown = New System.Windows.Forms.Button()
         Me.btn_modlistup = New System.Windows.Forms.Button()
         Me.lst_modslist = New System.Windows.Forms.ListBox()
-        Me.Settings = New System.Windows.Forms.TabPage()
-        Me.Dev = New System.Windows.Forms.TabPage()
-        Me.btn_removemod = New System.Windows.Forms.Button()
-        Me.btn_modname = New System.Windows.Forms.Button()
-        Me.Log = New System.Windows.Forms.TabPage()
+        Me.Prefabs = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
         Me.Merge = New System.Windows.Forms.TabPage()
-        Me.txt_setmodname = New System.Windows.Forms.TextBox()
-        Me.lst_log = New System.Windows.Forms.ListBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
-        Me.btn_mergemods = New System.Windows.Forms.Button()
         Me.btn_findmods = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn_mergemods = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Settings = New System.Windows.Forms.TabPage()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Dev = New System.Windows.Forms.TabPage()
         Me.btn_setpass = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Log = New System.Windows.Forms.TabPage()
+        Me.lst_log = New System.Windows.Forms.ListBox()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btn_mergelist = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
         Me.Loader.SuspendLayout()
+        Me.Prefabs.SuspendLayout()
+        Me.Merge.SuspendLayout()
+        Me.Settings.SuspendLayout()
         Me.Dev.SuspendLayout()
         Me.Log.SuspendLayout()
-        Me.Merge.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.Loader)
+        Me.TabControl1.Controls.Add(Me.Prefabs)
         Me.TabControl1.Controls.Add(Me.Merge)
         Me.TabControl1.Controls.Add(Me.Settings)
         Me.TabControl1.Controls.Add(Me.Dev)
@@ -65,6 +79,7 @@ Partial Class Main
         '
         'Loader
         '
+        Me.Loader.Controls.Add(Me.btn_mergelist)
         Me.Loader.Controls.Add(Me.txt_setmodname)
         Me.Loader.Controls.Add(Me.btn_modname)
         Me.Loader.Controls.Add(Me.btn_removemod)
@@ -79,6 +94,32 @@ Partial Class Main
         Me.Loader.TabIndex = 0
         Me.Loader.Text = "Loader"
         Me.Loader.UseVisualStyleBackColor = True
+        '
+        'txt_setmodname
+        '
+        Me.txt_setmodname.Location = New System.Drawing.Point(263, 96)
+        Me.txt_setmodname.Name = "txt_setmodname"
+        Me.txt_setmodname.Size = New System.Drawing.Size(183, 20)
+        Me.txt_setmodname.TabIndex = 8
+        Me.txt_setmodname.Text = "Insert Name Here"
+        '
+        'btn_modname
+        '
+        Me.btn_modname.Location = New System.Drawing.Point(263, 122)
+        Me.btn_modname.Name = "btn_modname"
+        Me.btn_modname.Size = New System.Drawing.Size(183, 32)
+        Me.btn_modname.TabIndex = 5
+        Me.btn_modname.Text = "Change mod name"
+        Me.btn_modname.UseVisualStyleBackColor = True
+        '
+        'btn_removemod
+        '
+        Me.btn_removemod.Location = New System.Drawing.Point(263, 58)
+        Me.btn_removemod.Name = "btn_removemod"
+        Me.btn_removemod.Size = New System.Drawing.Size(183, 32)
+        Me.btn_removemod.TabIndex = 4
+        Me.btn_removemod.Text = "Remove Mod"
+        Me.btn_removemod.UseVisualStyleBackColor = True
         '
         'btn_AddMod
         '
@@ -116,54 +157,44 @@ Partial Class Main
         Me.lst_modslist.Size = New System.Drawing.Size(250, 394)
         Me.lst_modslist.TabIndex = 0
         '
-        'Settings
+        'Prefabs
         '
-        Me.Settings.Location = New System.Drawing.Point(4, 22)
-        Me.Settings.Name = "Settings"
-        Me.Settings.Padding = New System.Windows.Forms.Padding(3)
-        Me.Settings.Size = New System.Drawing.Size(452, 412)
-        Me.Settings.TabIndex = 1
-        Me.Settings.Text = "Settings"
-        Me.Settings.UseVisualStyleBackColor = True
+        Me.Prefabs.Controls.Add(Me.TextBox2)
+        Me.Prefabs.Controls.Add(Me.Button1)
+        Me.Prefabs.Controls.Add(Me.ComboBox3)
+        Me.Prefabs.Location = New System.Drawing.Point(4, 22)
+        Me.Prefabs.Name = "Prefabs"
+        Me.Prefabs.Size = New System.Drawing.Size(452, 412)
+        Me.Prefabs.TabIndex = 5
+        Me.Prefabs.Text = "Prefabs"
+        Me.Prefabs.UseVisualStyleBackColor = True
         '
-        'Dev
+        'TextBox2
         '
-        Me.Dev.Controls.Add(Me.btn_setpass)
-        Me.Dev.Controls.Add(Me.TextBox1)
-        Me.Dev.Location = New System.Drawing.Point(4, 22)
-        Me.Dev.Name = "Dev"
-        Me.Dev.Size = New System.Drawing.Size(452, 412)
-        Me.Dev.TabIndex = 2
-        Me.Dev.Text = "Dev"
-        Me.Dev.UseVisualStyleBackColor = True
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox2.Location = New System.Drawing.Point(227, 34)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(222, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "Insert Name Here"
         '
-        'btn_removemod
+        'Button1
         '
-        Me.btn_removemod.Location = New System.Drawing.Point(263, 58)
-        Me.btn_removemod.Name = "btn_removemod"
-        Me.btn_removemod.Size = New System.Drawing.Size(183, 32)
-        Me.btn_removemod.TabIndex = 4
-        Me.btn_removemod.Text = "Remove Mod"
-        Me.btn_removemod.UseVisualStyleBackColor = True
+        Me.Button1.Location = New System.Drawing.Point(227, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(225, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Rename"
+        Me.Button1.UseVisualStyleBackColor = True
         '
-        'btn_modname
+        'ComboBox3
         '
-        Me.btn_modname.Location = New System.Drawing.Point(263, 122)
-        Me.btn_modname.Name = "btn_modname"
-        Me.btn_modname.Size = New System.Drawing.Size(183, 32)
-        Me.btn_modname.TabIndex = 5
-        Me.btn_modname.Text = "Change mod name"
-        Me.btn_modname.UseVisualStyleBackColor = True
-        '
-        'Log
-        '
-        Me.Log.Controls.Add(Me.lst_log)
-        Me.Log.Location = New System.Drawing.Point(4, 22)
-        Me.Log.Name = "Log"
-        Me.Log.Size = New System.Drawing.Size(452, 412)
-        Me.Log.TabIndex = 3
-        Me.Log.Text = "Log"
-        Me.Log.UseVisualStyleBackColor = True
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(4, 4)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(216, 21)
+        Me.ComboBox3.TabIndex = 0
         '
         'Merge
         '
@@ -178,39 +209,14 @@ Partial Class Main
         Me.Merge.Text = "Merge"
         Me.Merge.UseVisualStyleBackColor = True
         '
-        'txt_setmodname
+        'btn_findmods
         '
-        Me.txt_setmodname.Location = New System.Drawing.Point(263, 96)
-        Me.txt_setmodname.Name = "txt_setmodname"
-        Me.txt_setmodname.Size = New System.Drawing.Size(183, 20)
-        Me.txt_setmodname.TabIndex = 8
-        Me.txt_setmodname.Text = "Insert Name Here"
-        '
-        'lst_log
-        '
-        Me.lst_log.FormattingEnabled = True
-        Me.lst_log.Location = New System.Drawing.Point(4, 4)
-        Me.lst_log.Name = "lst_log"
-        Me.lst_log.Size = New System.Drawing.Size(445, 407)
-        Me.lst_log.TabIndex = 0
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(4, 4)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(445, 21)
-        Me.ComboBox1.TabIndex = 0
-        '
-        'ComboBox2
-        '
-        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(4, 138)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(445, 21)
-        Me.ComboBox2.TabIndex = 1
+        Me.btn_findmods.Location = New System.Drawing.Point(4, 166)
+        Me.btn_findmods.Name = "btn_findmods"
+        Me.btn_findmods.Size = New System.Drawing.Size(445, 100)
+        Me.btn_findmods.TabIndex = 3
+        Me.btn_findmods.Text = "Find mods"
+        Me.btn_findmods.UseVisualStyleBackColor = True
         '
         'btn_mergemods
         '
@@ -221,21 +227,99 @@ Partial Class Main
         Me.btn_mergemods.Text = "Merge"
         Me.btn_mergemods.UseVisualStyleBackColor = True
         '
-        'btn_findmods
+        'ComboBox2
         '
-        Me.btn_findmods.Location = New System.Drawing.Point(4, 166)
-        Me.btn_findmods.Name = "btn_findmods"
-        Me.btn_findmods.Size = New System.Drawing.Size(445, 100)
-        Me.btn_findmods.TabIndex = 3
-        Me.btn_findmods.Text = "Find mods"
-        Me.btn_findmods.UseVisualStyleBackColor = True
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(4, 138)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(445, 21)
+        Me.ComboBox2.TabIndex = 1
         '
-        'TextBox1
+        'ComboBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(4, 4)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(445, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(4, 4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(445, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Settings
+        '
+        Me.Settings.Controls.Add(Me.CheckBox5)
+        Me.Settings.Controls.Add(Me.CheckBox4)
+        Me.Settings.Controls.Add(Me.CheckBox3)
+        Me.Settings.Controls.Add(Me.CheckBox2)
+        Me.Settings.Controls.Add(Me.CheckBox1)
+        Me.Settings.Location = New System.Drawing.Point(4, 22)
+        Me.Settings.Name = "Settings"
+        Me.Settings.Padding = New System.Windows.Forms.Padding(3)
+        Me.Settings.Size = New System.Drawing.Size(452, 412)
+        Me.Settings.TabIndex = 1
+        Me.Settings.Text = "Settings"
+        Me.Settings.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(6, 99)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox5.TabIndex = 4
+        Me.CheckBox5.Text = "CheckBox5"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(6, 76)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox4.TabIndex = 3
+        Me.CheckBox4.Text = "CheckBox4"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(6, 53)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "CheckBox3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 30)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 7)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Dev
+        '
+        Me.Dev.Controls.Add(Me.btn_setpass)
+        Me.Dev.Controls.Add(Me.TextBox1)
+        Me.Dev.Location = New System.Drawing.Point(4, 22)
+        Me.Dev.Name = "Dev"
+        Me.Dev.Size = New System.Drawing.Size(452, 412)
+        Me.Dev.TabIndex = 2
+        Me.Dev.Text = "Dev"
+        Me.Dev.UseVisualStyleBackColor = True
         '
         'btn_setpass
         '
@@ -246,12 +330,45 @@ Partial Class Main
         Me.btn_setpass.Text = "Set Password"
         Me.btn_setpass.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(4, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(445, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Log
+        '
+        Me.Log.Controls.Add(Me.lst_log)
+        Me.Log.Location = New System.Drawing.Point(4, 22)
+        Me.Log.Name = "Log"
+        Me.Log.Size = New System.Drawing.Size(452, 412)
+        Me.Log.TabIndex = 3
+        Me.Log.Text = "Log"
+        Me.Log.UseVisualStyleBackColor = True
+        '
+        'lst_log
+        '
+        Me.lst_log.FormattingEnabled = True
+        Me.lst_log.Location = New System.Drawing.Point(4, 4)
+        Me.lst_log.Name = "lst_log"
+        Me.lst_log.Size = New System.Drawing.Size(445, 407)
+        Me.lst_log.TabIndex = 0
+        '
         'OpenFileDialog1
         '
-        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
-        Me.OpenFileDialog1.Filter = """Jet files (*.jet)|*.jet"""
+        Me.OpenFileDialog1.Filter = """Jet Files (*.jet)|*.jet"""
         Me.OpenFileDialog1.InitialDirectory = "C:\"
         Me.OpenFileDialog1.Title = "Please select a file"
+        '
+        'btn_mergelist
+        '
+        Me.btn_mergelist.Location = New System.Drawing.Point(263, 161)
+        Me.btn_mergelist.Name = "btn_mergelist"
+        Me.btn_mergelist.Size = New System.Drawing.Size(183, 32)
+        Me.btn_mergelist.TabIndex = 9
+        Me.btn_mergelist.Text = "Merge mod list."
+        Me.btn_mergelist.UseVisualStyleBackColor = True
         '
         'Main
         '
@@ -259,15 +376,21 @@ Partial Class Main
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 462)
         Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Bloons Mod Loader"
         Me.TabControl1.ResumeLayout(False)
         Me.Loader.ResumeLayout(False)
         Me.Loader.PerformLayout()
+        Me.Prefabs.ResumeLayout(False)
+        Me.Prefabs.PerformLayout()
+        Me.Merge.ResumeLayout(False)
+        Me.Settings.ResumeLayout(False)
+        Me.Settings.PerformLayout()
         Me.Dev.ResumeLayout(False)
         Me.Dev.PerformLayout()
         Me.Log.ResumeLayout(False)
-        Me.Merge.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -293,4 +416,14 @@ Partial Class Main
     Friend WithEvents btn_setpass As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Prefabs As TabPage
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents btn_mergelist As Button
 End Class
--- WindowsApplication2/Main.vb
+++ WindowsApplication2/Main.vb
@@ -1,6 +1,7 @@
-﻿Imports System.IO
-Public Class Main
+﻿
 
+Public Class Main
+    Dim x As Integer
     Private Sub btn_modlistup_Click(sender As Object, e As EventArgs) Handles btn_modlistup.Click 'Moves up the selected item
 
         If lst_modslist.SelectedIndex > 0 Then 'Make sure our item is not the first one on the list.
@@ -24,10 +25,46 @@ Public Class Main
     End Sub
 
     Private Sub btn_AddMod_Click(sender As Object, e As EventArgs) Handles btn_AddMod.Click
-        Dim UserprofilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-        Dim Downloadspath As String = UserprofilePath.Insert(0, "Test")
-        ' OpenFileDialog1.InitialDirectory = Downloadspath
-        txt_setmodname.Text = UserprofilePath
-        'OpenFileDialog1.ShowDialog()
+
+        Using ofd As New OpenFileDialog 'Shortens OpenfileDialog to ofd
+            Dim DownloadsPath = My.Computer.Registry.GetValue( 'Find the path to the downloads folder
+                "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\", "{374DE290-123F-4565-9164-39C4925E467B}", Nothing) ' Finds the registry value of the download path
+            ofd.InitialDirectory = DownloadsPath 'the ofd opens at the downloads directory
+            ofd.Filter = "Jet files (*.jet)|*.jet|Mod files (*.mod)|*.mod" 'forces you to use .jet files
+            If ofd.ShowDialog() = DialogResult.OK Then
+                If  Then
+
+                End If
+                lst_log.Items.Add("Mod found at the directory " & ofd.FileName)
+                lst_modslist.Items.Add(System.IO.Path.GetFileName(ofd.FileName))
+                x += 1
+            End If
+        End Using
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+    End Sub
+
+    Private Sub btn_removemod_Click(sender As Object, e As EventArgs) Handles btn_removemod.Click
+        lst_modslist.Items.Remove(lst_modslist.SelectedIndex)
+        lst_log.Items.Add("Removing " & lst_modslist.SelectedItem & " from mod list")
+        x -= 1
+
+
+    End Sub
+
+    Private Sub btn_modname_Click(sender As Object, e As EventArgs) Handles btn_modname.Click
+
+    End Sub
+
+    Private Sub btn_mergelist_Click(sender As Object, e As EventArgs) Handles btn_mergelist.Click
+        lst_log.Items.Add("Merging " & x & " .jet/.mod files")
+        If x < 2 Then
+            MsgBox("Pleas enter atleast 2 items to be merged")
+        Else
+            lst_modslist.Items.Clear()
+            lst_log.Items.Add("This has not yet been programed.")
+        End If
     End Sub
 End Class
